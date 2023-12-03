d8be507ffc632ecb5cdb7e55c1b4a291eecfb264
Vihan Perera <vihanpereraux@gmail.com>
2023-12-03 14:10:54 +0000

    Update row one light positions

--- BrainMechanics/Assets/Scripts/Controls.cs
+++ BrainMechanics/Assets/Scripts/Controls.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+
 
 public class Controls : MonoBehaviour
 {
+    // light controlls
     public GameObject lightRowOne;
     public GameObject lightRowTwo;
     public GameObject lightRowThree;
@@ -17,27 +21,29 @@ public class Controls : MonoBehaviour
 
     private float rotationSpeed = .8f;
     private float rotationMagnitude = 10f;
+
+    public GameObject testPointLight;
+
+    // fog controlls
+    // public Color fogColor = Color.gray;
+    // public float fogDensity = 0.015f;
     
 
     void Start(){
+        // fetching all light game objects
         for (int i = 0; i < 6; i++){
             lightRowOneLights.Add(lightRowOne.transform.GetChild(i).gameObject);
             lightRowTwoLights.Add(lightRowTwo.transform.GetChild(i).gameObject);
             lightRowThreeLights.Add(lightRowThree.transform.GetChild(i).gameObject);
             lightRowFourLights.Add(lightRowFour.transform.GetChild(i).gameObject);
         }
+
         StartCoroutine(RunFunc2SInterval());
+        RenderSettings.fogMode = FogMode.Exponential;
+        StartCoroutine(RunFogControlFunc());
     }
 
     void Update(){
-        // float rotationAngle = (Mathf.Sin(Time.time * rotationSpeed) * rotationMagnitude) + 90;
-        // float rotationAngleInverse = (Mathf.Sin(Time.time * rotationSpeed) * (rotationMagnitude * -1)) + 90;
-        // for (int i = 0; i < lightRowOneLights.Count; i++){
-        //     lightRowOneLights[i].transform.rotation = Quaternion.Euler(rotationAngle, -90f, -90f);
-        //     lightRowTwoLights[i].transform.rotation = Quaternion.Euler(rotationAngleInverse, -90f, -90f);
-        //     lightRowThreeLights[i].transform.rotation = Quaternion.Euler(rotationAngle, -90f, -90f);
-        //     lightRowFourLights[i].transform.rotation = Quaternion.Euler(rotationAngle, -90f, -90f);
-        // }
     }
 
     private IEnumerator RunFunc2SInterval(){
@@ -46,18 +52,49 @@ public class Controls : MonoBehaviour
             yield return new WaitForSeconds(.16f);
         }
     }
-    private void ControlLightRowOne(){
-        int lowerValue = 0;
-        int upperValue = 100;
-
-        for (int i = 0; i < 6; i++){
-            int randomValue = UnityEngine.Random.Range(lowerValue, upperValue);
-            int roundedValue = Mathf.RoundToInt(randomValue);
-
-            lightRowOneLights[i].GetComponent<Light>().intensity = roundedValue;
-            lightRowTwoLights[i].GetComponent<Light>().intensity = roundedValue;
-            lightRowThreeLights[i].GetComponent<Light>().intensity = roundedValue;
-            lightRowFourLights[i].GetComponent<Light>().intensity = roundedValue;
+    private IEnumerator RunFogControlFunc(){
+        while (true){
+            ControlFog();
+            yield return new WaitForSeconds(.1f);
         }
+    }
+
+    private void ControlLightRowOne(){
+        float testLightStarterPosition = -13f;
+        int lowerIndex = 0;
+        int upperIndex = 6;
+        int randomIndex = UnityEngine.Random.Range(lowerIndex, upperIndex);
+
+        // setting intensity for 0
+        for (int i = 0; i < lightRowOneLights.Count; i++){
+            lightRowOneLights[i].GetComponent<Light>().intensity = 0;
+        }        
+
+        lightRowOneLights[randomIndex].GetComponent<Light>().intensity = 100;
+        switch (randomIndex){
+            case 0:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+            case 1:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+            case 2:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+            case 3:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+            case 4:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+            case 5:
+                testPointLight.transform.position = new Vector3(testLightStarterPosition + (5 * randomIndex), 3, 0);
+            break;
+        }
+        Debug.Log(randomIndex);
+    }
+
+    private void ControlFog(){
+        RenderSettings.fogDensity = UnityEngine.Random.Range(0.0f, 0.015f);
     }
 }
